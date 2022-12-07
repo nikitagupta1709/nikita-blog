3b834b74c8d2aefcbb5d9b7d5d5b687a5dfa582f
Nikita Gupta <nikitagupta1709@gmail.com>
2022-12-08 00:48:32 +0530

    almost finished

--- react-blog/src/Pages/settings/Settings.jsx
+++ react-blog/src/Pages/settings/Settings.jsx
@@ -1,39 +1,79 @@
-import './settings.css'
-import { Sidebar } from '../../components/sidebar/Sidebar'
-import { CgProfile } from 'react-icons/cg'
-import { useContext } from 'react'
-import { Context } from '../../context/context'
+import "./settings.css"
+import {Sidebar} from "../../components/sidebar/Sidebar"
+import { CgProfile } from 'react-icons/cg';
+import { useContext, useState } from "react";
+import { Context } from "../../context/context";
+import axios from "axios";
 
 export const Settings = () => {
   const {user} = useContext(Context)
+  const [file, setFile] = useState(null);
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [success, setSuccess] = useState(false);
+
+  const handleSubmit = async (e)=>{
+    e.preventDefault();
+    let updatesUser = {
+        userId : user._id,
+        username,
+        email,
+        password
+    }
+    if(file){
+        const data = new FormData();
+        const filename = Date.now() + file.name;
+        data.append("name",filename);
+        data.append("file",file);
+        updatesUser.profilePic = filename;
+        try{
+            await axios.post("http://localhost:3050/upload", data)
+            
+        } catch(err) {
+            console.log(err,"err")
+        }
+    }
+
+    try {
+        await axios.put(`http://localhost:3050/users/${user._id}`, updatesUser)
+        setSuccess(true);
+    } catch (error) {
+        console.log(error,"error")
+    }
+}
 
   return (
-    <div className='settings'>
-      <div className="settingsWrapper"> 
+    <div className="settings">
+      <div className="settingsWrapper">
         <div className="settingsTitle">
-            <span className="settingsUpdateTitle">Update Your Account </span>
-            <span className="settingsDeleteTitle">Delete your Account</span>
+            <span className="settingsUpdateTitle">Update Your Account</span>
+            <span className="settingsDeleteTitle">Delete Account</span>
         </div>
-        <form className='settingsForm'>
+        <form className="settingsForm" onSubmit={handleSubmit}>
             <label>Profile Picture</label>
             <div className="settingsPP">
-                <img src={user.profilePic} alt=""/>
+                <img  src={user.profilePic} alt="" />
                 <label htmlFor="fileInput">
-                    <CgProfile className='settingsPPIcon'/>
+                    <CgProfile className="settingsPPIcon"/>
                 </label>
-                <input type="file" id="fileInput" style={{display:"none"}}/>
+                <input type="file" id="fileInput" style={{display:"none"}} onChange={(e)=>setFile(e.target.files[0])}/>
             </div>
-            <label>Username</label>
-            <input type="text" placeholder="Username"/>
-            <label>Email</label>
-            <input type="email" placeholder="example@gmail.com"/>
-            <label>Password</label>
-            <input type="password" />
-            <button className='settingsSubmit'>Update</button>
+            <label htmlFor="">Username</label>
+            <input type="text" placeholder={user.username} onChange={(e)=>setUsername(e.target.value)}/>
+            <label htmlFor="">Email</label>
+            <input type="email" placeholder={user.email} onChange={(e)=>setEmail(e.target.value)}/>
+            <label htmlFor="">Password</label>
+            <input type="password" onChange={(e)=>setPassword(e.target.value)} />
+            <button className="settingsSubmit" type="submit">Update</button>
+            {
+              success && (
+                <span style={{color: "green", textAlign:"center", marginTop:"20px"}}> Profile update </span>
+              )
+            }
         </form>
       </div>
-        <Sidebar />
+      <Sidebar />
     </div>
   )
 }
-
