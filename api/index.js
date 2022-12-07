ca9488690697fbada34f2c936b51eeac56090b98
Nikita Gupta <nikitagupta1709@gmail.com>
2022-12-07 12:18:43 +0530

    api

--- api/index.js
+++ api/index.js
@@ -3,6 +3,10 @@ import cors from "cors";
 import dotenv from 'dotenv'
 import mongoose from "mongoose"
 import authRoute from "./routes/auth.route.js"
+import usersRoute from  "./routes/users.route.js"
+import postRoute from "./routes/posts.route.js"
+import categoriesRoute from "./routes/categories.route.js"
+import multer from "multer"
 
 dotenv.config();
 const app = express();
@@ -18,13 +22,32 @@ const connection = async () =>{
             useNewUrlParser : true,
             useUnifiedTopology: true,});
         console.log("Connection established");
-        
     } catch (error) {
         console.log(error)
     }
 }
 
+const storage = multer.diskStorage({
+    destination:(req,file,callbackFn) =>{
+        callbackFn(null,"images")
+    }, 
+    filename:(req,file,callbackFn)=>{
+        callbackFn(null,req.body.name)
+    }
+})
+
+const upload =  multer({storage:storage});
+app.post("/upload", upload.single("file"),(req,res) =>{
+    res.send({
+        status: true,
+        message:"File has been uploaded"
+    });
+});
+
 app.use("/auth", authRoute);
+app.use("/users",usersRoute);
+app.use("/posts",postRoute);
+app.use("/categories",categoriesRoute);
 
 app.listen(3050, ()=>{
     try{
--- react-blog/src/Pages/home/Home.jsx
+++ react-blog/src/Pages/home/Home.jsx
@@ -1,9 +1,18 @@
-import './home.css'
+import { useEffect, useState } from 'react'
 import { Header } from '../../components/header/Header'
 import { Posts } from '../../components/posts/Posts'
 import { Sidebar } from '../../components/sidebar/Sidebar'
+import axios from 'axios'
+import './home.css'
 
 export const Home = () => {
+  const [post, setPost ] = useState([]);
+
+  useEffect(()=>{
+
+    axios.get()
+
+  }, [])
   return (
     <div>
         <Header />
